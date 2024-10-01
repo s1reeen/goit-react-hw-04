0aa44974988d4445ebc8df53428876feffb39ae6
s1reeen <s1reeen17@gmail.com>
2024-10-02 01:43:51 +0300

    api-intellegence-add

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchArticles = async () => {
   const { data } = await axios.get("https://api.unsplash.com/search/photos", {
-    params: { query: "maiden" },
+    params: { query: "nature" },
     headers: {
       Authorization: `Client-ID Yp3sVTN3Ytpow4IutzWGaqia3-qUV8btXkAoy5g_xxo`,
     },
